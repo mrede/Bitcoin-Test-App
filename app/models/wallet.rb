553072fc4dba266d3e723994065c4ca03b8b8172
Ben Ede <ben.ede@gmail.com>
2015-05-30 16:55:16 -0700

    added unique name

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 class Wallet < ActiveRecord::Base
-	validates_presence_of :private_key
+	validates_presence_of :private_key, :public_key, :name
+
+	validates_uniqueness_of :name
 end
--- spec/models/wallet_spec.rb
+++ spec/models/wallet_spec.rb
@@ -5,4 +5,6 @@ RSpec.describe Wallet, type: :model do
   it { should validate_presence_of(:private_key) }
   it { should validate_presence_of(:public_key) }
   it { should validate_presence_of(:name) }
+
+  it { should validate_uniqueness_of(:name) }
 end
