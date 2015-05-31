8fc6144a8daeb4b50213e9f45aaa719a7d15f31f
Ben Ede <ben.ede@gmail.com>
2015-05-30 20:04:11 -0700

    added has _many relation

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -4,4 +4,6 @@ class Wallet < ActiveRecord::Base
 	validates_presence_of :private_key, :public_key, :name
 
 	validates_uniqueness_of :name
+
+  has_many :addresses
 end
--- spec/models/wallet_spec.rb
+++ spec/models/wallet_spec.rb
@@ -7,4 +7,6 @@ RSpec.describe Wallet, type: :model do
   it { should validate_presence_of(:name) }
 
   it { should validate_uniqueness_of(:name) }
+
+  it { should have_many(:addresses)}
 end
