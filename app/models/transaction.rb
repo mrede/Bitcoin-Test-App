a2f7f67dedf0f48c0392c8be6549698920dbc0f8
Ben Ede <ben.ede@gmail.com>
2015-05-30 21:16:42 -0700

    added create_from_tx

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,4 +2,17 @@ class Transaction < ActiveRecord::Base
 
   validates_presence_of :unique_key, :value, :raw
   belongs_to :address
+
+
+  def self.create_from_tx(tx, address)
+    t = Transaction.new
+    t.unique_key = tx.hash
+    t.address = address
+    t.value = 123
+    t.raw = 123
+    res = t.save
+
+    puts "TRAN #{t.id} = #{res} #{t.errors.to_hash}"
+
+  end
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -5,4 +5,21 @@ RSpec.describe Transaction, type: :model do
   it { should validate_presence_of(:raw) }
   it { should validate_presence_of(:value) }
   it { should validate_presence_of(:unique_key) }
+
+  describe "#create_from_tx" do
+    context "when data is valid" do
+      it "should create a new transction" do
+        expect {
+
+          tx = double("tx")
+          allow(tx).to receive(:hash).and_return("1234567890")
+
+          address = create(:address)
+          t= Transaction.create_from_tx(tx, address)
+
+          }.to change(Transaction, :count).by(1)
+      end
+    end
+    context "when data is invalid"
+  end
 end
