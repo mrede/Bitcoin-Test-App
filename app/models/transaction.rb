df50a192d7537179dc860e7043ece4e74832ca02
Ben Ede <ben.ede@gmail.com>
2015-06-01 16:10:46 -0700

    fixed relation

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,6 +3,6 @@ class Transaction < ActiveRecord::Base
   validates_presence_of :unique_hash, :original_json
   validates_uniqueness_of :unique_hash
   has_many :outputs
-  has_many :inputs, class_name: "Output"
+  has_many :inputs, class_name: "Output", foreign_key: "as_transaction_input_id"
 
 end
