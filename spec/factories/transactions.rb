b29b58fded93db7d362e5d44bddd77a3d44e0a02
Ben Ede <ben.ede@gmail.com>
2015-06-01 11:41:07 -0700

    transaction factory now uses realistic random hash

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
-  factory :transaction do
-    hash "MyString"
+  factory :transaction, aliases: [:owner_transaction, :as_transaction_input] do
+    unique_hash {(0...50).map { ('a'..'z').to_a[rand(26)] }.join}
     
-    original_json ""
+    original_json "{ none: '' }"
     
   end
 
