8b0af9e584d41814596c44a5952486f0df53475e
Ben Ede <ben.ede@gmail.com>
2015-05-31 20:17:33 -0700

    added amount_mtbc

--- spec/factories/outputs.rb
+++ spec/factories/outputs.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :output do
     transaction_id 1
-address_id 1
-value 1
+    address_id 1
+    value {rand(1..1000)* 100000} 
   end
 
 end
--- spec/models/output_spec.rb
+++ spec/models/output_spec.rb
@@ -3,4 +3,15 @@ require 'rails_helper'
 RSpec.describe Output, type: :model do
   it { should belong_to(:owner_transaction) }
   it { should belong_to(:address) }
+
+  describe "#amount_mbtc" do
+    it "should display value in mBTC" do
+      output = create(:output, value: 100000000)
+      expect(output.value_mbtc).to eq(1000)
+    end
+    it "should handle zero value" do
+      output = create(:output, value: 0)
+      expect(output.value_mbtc).to eq(0)
+    end
+  end
 end
