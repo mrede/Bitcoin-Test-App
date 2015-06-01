cc05c2cddefc10a139d5eea3b5741ed027818829
Ben Ede <ben.ede@gmail.com>
2015-05-31 19:18:37 -0700

    to_s

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -4,4 +4,8 @@ class Wallet < ActiveRecord::Base
 	validates_uniqueness_of :name
 
   has_many :addresses
+
+  def to_s
+    self.name
+  end
 end
--- spec/models/wallet_spec.rb
+++ spec/models/wallet_spec.rb
@@ -9,4 +9,9 @@ RSpec.describe Wallet, type: :model do
   it { should validate_uniqueness_of(:name) }
 
   it { should have_many(:addresses)}
+
+  it "returns the string name" do
+    wallet = create(:wallet)
+    expect(wallet.to_s).to eq(wallet.name)
+  end
 end
