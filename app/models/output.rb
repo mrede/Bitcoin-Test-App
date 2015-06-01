19c1dbd500987fb7d0e7b658b37cd5f997a8631f
Ben Ede <ben.ede@gmail.com>
2015-05-31 20:17:18 -0700

    added value_mtbc

--- app/models/output.rb
+++ app/models/output.rb
@@ -1,4 +1,8 @@
 class Output < ActiveRecord::Base
   belongs_to :owner_transaction, foreign_key: "transaction_id", class_name: "Transaction"
   belongs_to :address
+
+  def value_mbtc
+    self.value / 100000
+  end
 end
