afe519a42387253c658063e85ed722d2d5e34b01
Ben Ede <ben.ede@gmail.com>
2015-06-01 18:41:05 -0700

    added new create_from_tx

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -5,4 +5,14 @@ class Transaction < ActiveRecord::Base
   has_many :outputs
   has_many :inputs, class_name: "Output", foreign_key: "as_transaction_input_id"
 
+  def self.create_from_tx(inputs, new_tx)
+    return false unless !new_tx.nil?
+    trans = Transaction.new
+    trans.inputs << inputs
+    trans.original_json = new_tx.to_json
+    trans.unique_hash = new_tx.hash
+    # We don't need to record outputs as they will appear from server
+    trans.save
+
+  end
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -11,5 +11,29 @@ RSpec.describe Transaction, type: :model do
 
   it { should validate_uniqueness_of(:unique_hash)}
 
-  
+  describe "#create_from_tx" do
+    before(:each) do
+      @trans = create(:transaction)
+      @tx = Bitcoin::P::Tx.from_json(@trans.original_json)
+      @output = create(:output, owner_transaction: @trans)
+      @inputs = [@output]
+    end
+
+    it "handles empty tx" do
+
+      res = Transaction.create_from_tx(@inputs, nil)
+      expect(res).to equal(false)
+
+    end
+    it "handles empty inputs" do
+      expect {
+        Transaction.create_from_tx([], @tx)
+      }.to change(Transaction, :count).by(1)
+    end
+    it "creates transaction if inputs and tx is ok" do
+      expect {
+        Transaction.create_from_tx(@inputs, @tx)
+      }.to change(Transaction, :count).by(1)
+    end
+  end
 end
