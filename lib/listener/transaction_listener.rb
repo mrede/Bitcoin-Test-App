18e29b9a64bdba96f793114cfc77bf3b059a17fd
Ben Ede <ben.ede@gmail.com>
2015-06-01 20:31:29 -0700

    added detection of Block to confirm transaction

--- lib/listener/transaction_listener.rb
+++ lib/listener/transaction_listener.rb
@@ -53,6 +53,25 @@ class TransactionListener < Bitcoin::Connection
   def on_ping(x)
   end
 
+  def on_block(block)
+    p ['block', block.bip34_block_height]
+
+    block.tx.each do |t|
+      p [Time.now, 'Trans in block:  ', t.hash]
+      # Check block
+      trans = Transaction.where(unique_hash: t.hash).first
+      unless trans.nil?
+        puts "FOUND TRANS"
+        if trans.confirmed.nil?
+          trans.confirmed = 0
+        end
+        trans.confirmed += 1
+        trans.save
+      end
+    end
+    #p block.payload.each_byte.map{|i| "%02x" % [i] }.join(" ")
+    #puts block.to_json
+  end
   
 
 end
